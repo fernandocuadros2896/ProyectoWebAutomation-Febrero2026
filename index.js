0cf5461243cdf7f53ac6eb800ea60bd65073e254
fernandocuadros2896 <fernandocuadros2896@gmail.com>
2026-04-12 21:11:13 -0500

    reporte2

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const report = require('multiple-cucumber-html-reporter');
+
 report.generate({
-    jsonDir: 'target/cucumber-report/',
-    reportPath: 'target/cucumber-html-report/',
-    metadata:{
-        browser: {
-            name: 'chrome',
-            version: 'latest'
-        },
-        device: 'Windows',
-        platform: {
-            name: 'Windows'
-        }
+  jsonDir: 'target/cucumber-report', // carpeta donde está el JSON
+  reportPath: 'target/cucumber-html-report',
+  displayDuration: true,// salida del HTML
+  metadata: {
+    browser: {
+      name: 'chrome',
+      version: 'latest'
     },
-    customData: {
-        title: 'Run info',
-        data: [
-            {label: 'Project', value: 'Proyecto Selenium POM'},
-            {label: 'Generated on', value: new Date().toLocaleString()}
-        ]
+    device: 'Local test machine',
+    platform: {
+      name: process.platform,
+      version: process.version
     }
+  },
+  customData: {
+    title: 'Test Execution Info',
+    data: [
+      { label: 'Proyecto', value: 'Proyecto POM' },
+      { label: 'Ejecución', value: new Date().toLocaleString() }
+    ]
+  }
 });
